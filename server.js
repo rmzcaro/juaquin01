b924bab4c7a2521290a70c73c67cc9588707ab1b
Carolina RC <caro.rmz315@gmail.com>
2018-09-10 16:15:36 -0500

    added server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-// const express = require("express");
-// const app = express();
-// let PORT = process.env.PORT || 3000; 
+const express = require("express");
+const app = express();
+let PORT = process.env.PORT || 3000; 
 
-// app.use(express.static("public"))
-// app.get("/", (req,res)=>{
-//     res.sendFile("./public/index.html")
-// })
-// app.listen(PORT, ()=>{
-//     console.log("app is running")
-// })
+app.use(express.static("public"))
+app.get("/", (req,res)=>{
+    res.sendFile("./public/index.html")
+})
+app.listen(PORT, ()=>{
+    console.log("app is running")
+})
